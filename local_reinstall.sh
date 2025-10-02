58e05d1d17fb8c7808f10af1eb7fa4921d477b52
Robin Stammer <robin.stammer@posteo.de>
2025-10-02 10:19:26 +0200

    Increase version number and refine bundler setup

--- local_reinstall.sh
+++ local_reinstall.sh
@@ -1,1 +1,1 @@
-gem build rubythinking.gemspec && gem install rubythinking-0.1.0.gem && iruby register --force
+gem build rubythinking.gemspec && gem install rubythinking-0.3.0.gem && iruby register --force
